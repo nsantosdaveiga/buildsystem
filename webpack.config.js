b39762ae40a1cc5db5ee0183d17a9bfb550ee286
nsantosdaveiga <nelson.santosdaveiga@gmail.com>
2016-09-21 10:34:04 +0200

    Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,26 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: ['./src/Style/style.scss','./src/JavaScript/main.js'],
+    entry: [
+        './src/Style/style.scss',
+        './src/JavaScript/main.js'
+    ],
     output: {
         path: './public',
-        filename: 'js/main.js',
+        filename: 'assets/js/main.js'
     },
     module: {
         loaders: [
             {test: /\.jade$/, loader: "jade"},
             {test: /\.scss$/, loader: ExtractTextPlugin.extract('css!sass')},
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel', // 'babel-loader' is also a valid name to reference
+                query: {
+                    presets: ['es2015']
+                }
+            }
         ]
     },
     plugins: [
@@ -18,6 +29,6 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.jade'
         }),
-        new ExtractTextPlugin("./style/style.css")
+        new ExtractTextPlugin("./assets/style/style.css")
     ]
 }
